65e107dfd183387f4e2d3ecb40e0e5fe7e1c55a2
Keinerdmb <Keinermb01@gmail.com>
2026-03-05 13:47:43 -0500

    Agregue comentarios y organice el codigo

--- main.py
+++ main.py
@@ -1,94 +1,156 @@
 # Lógica de las opciones del menú
 
-while not Salir: 
+#---------------------------
+# Opcion 1: Depositar dinero
+#---------------------------
 
-
- opcion = input("Elija una opción : \n")
- if opcion == "1":
-     while True:
-         try:
-             deposito = float(input("Ingrese el monto a depositar:\n"))
-         except ValueError:
+if opcion == "1":
+    while True:
+        try:
+            deposito = float(input("Ingrese el monto a depositar:\n"))
+        except ValueError:
             print("Monto inválido. Ingrese un monto válido.")
             continue    
          
-         if deposito <=0:
-            print ("Monto inválido. Ingrese un monto valido")
+        if deposito <=0:
+            print("Monto inválido. Ingrese un monto valido")
             continue
 
      
-         saldo += deposito
-         movimientos.append(("Deposito", deposito)) 
-         print(f"Deposito realizado. Nuevo saldo: {saldo:.2f}\n")
-         break
+        saldo += deposito
+        movimientos.append(("Deposito", deposito)) 
+        print(f"\nDeposito realizado. Nuevo saldo: {saldo:.2f}")
+         
+        while True:
 
-    
-     print ("1. Volver al menú    2. Salir\n")
-     sub= input("Seleccione una opción:\n ")
+            print ("\n1. Realizar otro deposito")
+            print ("2. Volver al menú")
+            print ("3. Salir\n")
+            sub= input("Seleccione una opción:\n ")
 
-     if sub == "2":
-         print("Hasta luego!")
-         Salir = True 
+            if sub == "1":
+               break
+            
+            elif sub == "2":
+                break
 
- elif opcion == "2":
-     while True:
-         try:
-             retiro = float(input("Ingrese el monto a retirar: \n"))
-         except ValueError:
-             print("Monto invalido. Ingrese un monto válido.")
-             continue
+            elif sub == "3":
+                print("Hasta luego!")
+                Salir = True
+                break
+
+            else:
+                print("Opción no válida. Por favor, seleccione una opción del menú.\n")
+
+        if Salir or sub == "2":
+            break
+
+#---------------------------
+# Opcion 2: Retirar dinero
+#---------------------------
+
+elif opcion == "2":
+    while True:
+        try:
+            retiro = float(input("Ingrese el monto a retirar: \n"))
+        except ValueError:
+            print("Monto invalido. Ingrese un monto válido.")
+            continue
              
-         if retiro <= 0:
-             print("Monto inválido. Ingrese un monto válido.")
-             continue
+        if retiro <= 0:
+            print("Monto inválido. Ingrese un monto válido.")
+            continue
 
-         elif retiro > saldo:
-             print("Fondos insuficientes.")
+        elif retiro > saldo:
+            print("Fondos insuficientes.")
+            continue
        
      
-         saldo -= retiro 
-         movimientos.append(("Retiro", retiro))
-         print (f"Retiro realizado. Nuevo saldo: {saldo:.2f}")
-         break
-     
-     
-     print ("1. Volver al menú  2. Salir\n")
-     sub= input("Seleccione una opción:\n")
+        saldo -= retiro 
+        movimientos.append(("Retiro", retiro))
+        print(f"\nRetiro realizado. Nuevo saldo: {saldo:.2f}")
 
-     if sub =="2":
-        print ("Hasta luego!")
-        Salir= True
+        while True:
+            print ("\n1. Realizar otro retiro")
+            print ("2. Volver al menú")
+            print ("3. Salir\n")   
+            sub= input("Seleccione una opción:\n")
 
- elif opcion == "3":
+            if sub =="1":
+                break
+            elif sub == "2":
+                break
+
+            elif sub == "3":
+                print("Hasta luego!")
+                Salir = True
+                break
+
+            else:
+                print("Opción no válida. Por favor, seleccione una opción del menú.\n")
+            
+        if Salir or sub == "2":
+            break    
+
+#---------------------------
+# Opcion 3: Ver movimientos
+#---------------------------
+
+elif opcion == "3":
     
     if not movimientos:
         print("\nNo se han realizado movimientos.\n")
+
     else:   
         print("\n---Movimientos realizados:---") 
         for mov in movimientos:
             if mov [0] == "Retiro":
-              print(f"{mov[0]}: -{mov[1]:.2f}")
+                print(f"\n{mov[0]}: -{mov[1]:.2f}")
+
             else:
-              print(f"{mov[0]}: +{mov[1]:.2f}") 
-       
-    print ("\n1. Volver al menú  2. Salir\n")
+                print(f"\n{mov[0]}: +{mov[1]:.2f}") 
+
+        while True:    
+            print ("\n1. Volver al menú")
+            print ("2. Salir\n")
+            sub= input("Seleccione una opción:\n")
+
+            if sub == "1":
+                break
+        
+            elif sub == "2":
+                print("Hasta luego!")
+                Salir = True
+                break
+
+            else:
+                print("Opción no válida. Por favor, seleccione una opción del menú.\n")
+
+#---------------------------
+# Opcion 4: Ver saldo
+# ---------------------------  
+    
+elif opcion == "4":
+    print(f"\nSu saldo actual es: {saldo:.2f}")
+    print ("\n1. Volver al menú")
+    print ("2. Salir\n")
     sub= input("Seleccione una opción:\n")
 
     if sub == "2":
-       print("Hasta luego!")
-       Salir = True
-    
-    else:
-       
- elif opcion == "4":
-   print(f"Su saldo actual es: {saldo:.2f}\n")
-   print ("1. Volver al menú  2. Salir\n")
-   sub= input("Seleccione una opción:\n")
+        print("Hasta luego!")
+        Salir= True
 
-   if sub == "2":
-    print ("Hasta luego!")
-    Salir= True
+#---------------------------
+# Opcion 5: Salir
+#---------------------------
 
+elif opcion == "5":
+    print("Hasta luego!")
+    Salir = True      
 
- else:
-  print("Opción no válida. Por favor, seleccione una opción del menú.\n")    
+#---------------------------
+# Opcion no valida
+#---------------------------
+
+else:
+    print("Opción no válida. Por favor, seleccione una opción del menú.\n")    

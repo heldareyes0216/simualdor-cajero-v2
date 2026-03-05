6a89dd8d3e69bedc718cc0e8926e403cbf162683
Joan <estremorjoan@gmail.com>
2026-03-05 17:28:47 -0500

    mini-cambios

--- main.py
+++ main.py
@@ -10,12 +10,11 @@ intentos = 3
 saldo = 1000
 retiro = 0
 deposito = 0
-movimientos = {}
+movimientos = []
 salir = False
 
 def limpiar_pantalla():
  os.system('cls' if os.name == 'nt' else 'clear')
- salir = False
 
 while not salir:
     
@@ -26,7 +25,7 @@ while not salir:
     sys.stdout.write(f"\rCargando: |{barra}{espacios}| {i}%")
     sys.stdout.flush()
     time.sleep(0.02)
- limpiar_pantalla()
+    limpiar_pantalla()
  
  break
 
@@ -41,6 +40,9 @@ while intentos >0:
 
  if usuario == USUARIO_CORRECTO and clave == CLAVE_CORRETA:
   print("\n ✅ \033[32mAcceso concedido\033[0m""\n")
+
+  input("\nPresione ENTER para continuar...")
+  limpiar_pantalla()
   
   for i in range(101):
     barra = "█" * (i//2) #LLENARIA LA BARRA
@@ -49,20 +51,27 @@ while intentos >0:
     sys.stdout.write(f"\rCargando: |{barra}{espacios}| {i}%")
     sys.stdout.flush()
     time.sleep(0.01)
+    
   break
  
-else:
+ else:
   intentos -= 1
   print(f"❌ \033[31mDatos incorrectos.\033[0m Intentos restantes: {intentos}\n")
 
-if intentos == 0:
+ if intentos == 0:
   print("🚫 \033[31mCuenta Bloqueada\033[0m")
   exit()
-  limpiar_pantalla()
+
+limpiar_pantalla()
 
 #MENU
-print("    ""=== MENÚ===")
-print("-seleccione una operacion""\n")
-print("1. Depositar        2. Retirar")
-print("3. Saldo            4. Movimiento")
-
+while True:
+ 
+ print("    ""=== MENÚ===")
+ print("-seleccione una operacion\n")
+ print("1. Depositar        2. Retirar")
+ print("3. Saldo            4. Movimiento")
+ print("Salir\n")
+ 
+ opcion = input("\nSeleccione una opcion")
+ limpiar_pantalla()
